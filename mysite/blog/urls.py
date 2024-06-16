a448fc66db6c3e5a1c9e9c8807bc997f7fb69a08
Daniel <daniel.palacz@gmail.com>
2024-06-16 12:54:13 +0200

    extends 'base' template and implements new endpoints (/, projects/, contact/)

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.home, name='blog'),
-    path('<slug:post_slug>/', views.post_single, name='post_single'),
+    path('', views.home, name='home'),
+    path('contact/', views.contact_details, name='contact_details'),
+    path('projects/', views.projects, name='projects'),
+    path('blog/', views.blog, name='blog'),
+    path('blog/<slug:post_slug>/', views.post_single, name='post_single'),
 ]
