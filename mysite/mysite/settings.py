ddf52c450b697a696f50b655f30f8e9f84895e08
Daniel <daniel.palacz@gmail.com>
2024-10-10 12:03:23 +0200

    updates settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jh9bv7$bzg=_0^#5&s$!e7l9y9=srapr*-u2=ydx23*6w839=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["danielpalacz.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
